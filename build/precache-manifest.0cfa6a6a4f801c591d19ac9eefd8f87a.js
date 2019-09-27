1eaccaa69cbbe5751361f664f4ddd9163dd3c2b7
maoguijun <maoguijun@mini1.cn>
2019-09-27 18:44:08 +0800

    添加build

---
+++ build/precache-manifest.0cfa6a6a4f801c591d19ac9eefd8f87a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3205744ec8c7d4e49f5a",
+    "url": "/static/js/main.3205744e.chunk.js"
+  },
+  {
+    "revision": "29e7a19ce40c46eae61c",
+    "url": "/static/js/1.29e7a19c.chunk.js"
+  },
+  {
+    "revision": "3205744ec8c7d4e49f5a",
+    "url": "/static/css/main.3620af00.chunk.css"
+  },
+  {
+    "revision": "29e7a19ce40c46eae61c",
+    "url": "/static/css/1.848787fe.chunk.css"
+  },
+  {
+    "revision": "e5b1c5df595131d33a18d4d57abeac01",
+    "url": "/index.html"
+  }
+];
